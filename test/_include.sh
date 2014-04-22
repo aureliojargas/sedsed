21eb71d1305d303038f769da01b5b55f54004775
Aurelio Jargas <verde@aurelio.net>
2014-04-22 19:43:02 -0300

    New config+lib file test/_include.sh
    To centralize configuration and common functions used by the tests.

---
+++ test/_include.sh
@@ -0,0 +1,22 @@
+# Include file used by all the sedsed tests.
+
+sed='gsed'
+
+sedsed='python ../../sedsed.py'
+# sedsed='python2.6 ../../sedsed.py'
+# sedsed='python2.7 ../../sedsed.py'
+# sedsed='python3   ../../sedsed.py'
+
+sed_output='sed-output.txt'
+sedsed_output='sedsed-output.txt'
+
+text=$(printf "one\ntwo\nthree\nfour\nfive\nsix\n")
+
+tests_clean_up() {
+    rm -f $sed_output $sedsed_output w.out1 w.out2
+}
+
+tests_git_status() {
+    # Use git to show the errors (differences)
+    git status --short . | sed 's/^/********** ERROR (use git diff): /'
+}
