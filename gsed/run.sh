dbc6353ca7dc1bec2fa6b093153ade224a600620
Aurelio Jargas <verde@aurelio.net>
2019-07-17 01:04:43 +0200

    run: add sed.sf.net scripts

--- gsed/run.sh
+++ gsed/run.sh
@@ -12,3 +12,19 @@ cat ../test/{parsing,scripts}/*.sed > blob.sed
 ../sedsed.py -i -f blob.sed > blob-sedsed.sed
 python3 compile_c.py blob.sed | sed "1,/ch='<EOF>'$/ d" > blob-gsed.sed
 diff -u blob-{sedsed,gsed}.sed | less
+
+# all sed.sf.net scripts
+find ../../sed.sf.net/ -name '*.sed' |
+    grep -v \
+        -e sedermind.sed$ \
+        -e untroff.sed$ \
+        -e html_uc.sed$ \
+        -e html_lc.sed$ \
+        -e iso2html.sed$ \
+        -e html2iso.sed$ \
+        -e indexhtml.sed$ \
+        -e sodelnum.sed$ |
+    xargs -n 1 cat > blob2.sed
+../sedsed.py -i -f blob2.sed > blob2-sedsed.sed
+python3 compile_c.py blob2.sed | sed "1,/ch='<EOF>'$/ d" > blob2-gsed.sed
+diff -u blob2-{sedsed,gsed}.sed | less
