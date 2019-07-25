d65b66e46d7d25bca12386756c8d501a74dcbb05
Aurelio Jargas <verde@aurelio.net>
2019-07-26 01:02:05 +0200

    Improve run.sh

--- gsed/run.sh
+++ gsed/run.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+# Compare output from old sedsed parser to the ported GNU sed parser
+#
+# Differences:
+#
+#   1. Partial line comments in the output are not supported
+#      Old: p       ;# comment
+#      New: p
+#           # comment
+#
+#   2. Using \n as the s delimiter is not supported by GNU sed.
+#      If seems it was never really supported by GNU sed.
+#      This is used in one old script: indexhtml.sed
+#
+#   3. Old parser had a bug which inserted an extra space in the start
+#      of a/i/c commands.
+#      Old: i\
+#            foo
+#      New: i\
+#           foo
+#
+#   4. Command r now considers any char (except \n) as part of the filename.
+#      Old parser stopped at blanks.
+#      Old: r foo
+#      New: r foo bar baz
+
 
 # remove space after b and t without labels
 fix_sedsed_b_t() {
@@ -39,7 +64,11 @@ diff -u blob-{sedsed,gsed}.sed | less
 # all sed.sf.net scripts
 find ../../sed.sf.net/ -name '*.sed' |
     remove_broken_scripts |
-    xargs -n 1 cat > blob2.sed
+    while read file
+    do
+        cat "$file"
+        echo
+    done > blob2.sed
 ../sedsed.py -i -f blob2.sed | fix_sedsed_b_t > blob2-sedsed.sed
 python3 compile_c.py blob2.sed | remove_debug > blob2-gsed.sed
 diff -u blob2-{sedsed,gsed}.sed | less
