b8b7649ec8a500ff0f006beb54c28b6832ebde98
Aurelio Jargas <verde@aurelio.net>
2019-08-29 12:07:09 +0200

    unified gnused.py

--- gsed/utils_h.py
+++
@@ -1,5 +0,0 @@
-# enum exit_codes {
-EXIT_SUCCESS = 0            # is already defined as 0
-EXIT_BAD_USAGE = 1          # bad program syntax, invalid command-line options
-EXIT_BAD_INPUT = 2          # failed to open some of the input files
-EXIT_PANIC = 4              # PANIC during program execution
